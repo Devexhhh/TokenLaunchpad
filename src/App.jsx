0b367af0a4166a1f7b2e98408bdcc3fa229383bd
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-13 23:02:06 +0530

    imported wallet adapters

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css"
+
 import { TokenLaunchpad } from "./components/TokenLaunchpad";
 
 const App = () => {
   return (
     <>
-      <TokenLaunchpad />
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <div className="bg-zinc-800 h-screen w-max-7xl py-5">
+              <div className="flex justify-between px-20">
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+              <TokenLaunchpad />
+            </div>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </>
   )
 }
