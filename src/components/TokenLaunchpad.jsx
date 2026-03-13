9dc84f5e254f91b3cd44834afc001993d81fa2f3
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-13 23:02:01 +0530

    fixed UI

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -7,7 +7,7 @@ export const TokenLaunchpad = () => {
                 <input className="inputText placeholder:text-white text-white border px-10 py-2 rounded-xl" type="text" placeholder="Symbol"></input> <br />
                 <input className="inputText placeholder:text-white text-white border px-10 py-2 rounded-xl" type="text" placeholder="Image URL"></input> <br />
                 <input className="inputText placeholder:text-white text-white border px-10 py-2 rounded-xl" type="text" placeholder="Initial Supply"></input> <br />
-                <button className="btn border placeholder:text-white text-white px-5 py-2 cursor-pointer rounded-xl bg-violet-500 text-white">Create a Token</button>
+                <button className="btn border placeholder:text-white text-white px-5 py-2 cursor-pointer rounded-xl bg-violet-500">Create a Token</button>
             </div >
         </>
     )
